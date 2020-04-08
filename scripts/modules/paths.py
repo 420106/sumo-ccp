5747f5eaeace4601c95d33107dcbd0a708640b96
420106 <31180186+420106@users.noreply.github.com>
2020-04-08 11:29:03 +1000

    folder naming changes

--- scripts/modules/paths.py
+++ scripts/modules/paths.py
@@ -15,22 +15,22 @@ report_template = f'{root}scripts\\ref\\report_template.csv'
 '''
     Cloud
 '''
-s3 = 'C:\\Users\\sliu\\Documents\\GitHub\\sumo-ccp\\test\\' # Test path
+files = 'C:\\Users\\sliu\\Documents\\GitHub\\sumo-ccp\\files\\' # Test path
 
 # XMLs
-xmls = f'{s3}xmls\\'
+xmls = f'{files}xmls\\'
 invoice = f'{xmls}invoice\\' # Invoice; Final Invoice; Reminder Notice; Final Notice (Disconnection Notice)
 welcome_pack = f'{xmls}welcome_pack\\' # Welcome Pack; Contract Novation
 transfer_letter = f'{xmls}transfer_letter\\' # Transfer Letter
 payment_plan = f'{xmls}payment_plan\\' # Payment Plan
 
 # PDFs
-pdfs = f'{s3}pdfs\\'
+pdfs = f'{files}pdfs\\'
 
 # Logs
-download_logs = f'{s3}logs\\download\\'
-upload_logs = f'{s3}logs\\upload\\'
+download_logs = f'{files}logs\\download\\'
+upload_logs = f'{files}logs\\upload\\'
 
 # Reports
-reports = f'{s3}reports\\'
+reports = f'{files}reports\\'
 # ---------------------------------------------------------------------------------------------------------
